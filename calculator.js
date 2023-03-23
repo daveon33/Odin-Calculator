773d2fc74a50606653ea4b87c8a65db03cff2316
David Gonzalez <davidgonzalez5910@gmail.com>
2023-03-22 19:36:55 -0500

    Reestructured some buttons functionality

--- calculator.js
+++ calculator.js
@@ -48,9 +48,42 @@ function operate(operator, a, b) {
 // Targeting the number buttons
 
 const screenDisplay = document.querySelector('#displayContent');
-const newPress = document.querySelectorAll(".numberBtns");
+const newPress = document.querySelectorAll(".numeric");
 newPress.forEach(btn => {
     btn.addEventListener('click', e => {
-        screenDisplay.innerHTML = e.target.textContent;
+        screenDisplay.innerHTML += e.target.textContent;
     });
+});
+
+// Targeting the operation buttons
+
+const operationPress = document.querySelectorAll('.operative');
+operationPress.forEach(btn => {
+    btn.addEventListener('click', e => {
+        screenDisplay.innerHTML += e.target.textContent;
+        firstValue = screenDisplay.innerHTML.substring(0, screenDisplay.innerHTML.length - 1);
+        option = screenDisplay.innerHTML.substring(screenDisplay.innerHTML.length - 1);
+    });
+});
+
+// Make the operation happen
+
+const enterPress = document.querySelector('#enter');
+enterPress.addEventListener('click', e => {
+secondValue = screenDisplay.innerHTML.match('[\+\-\*\/]\d+')[0];
+    console.log(secondValue);
+});
+
+// Make the clear button reset the display
+
+const clearPress = document.querySelector('#clear');
+clearPress.addEventListener('click', e => {
+    screenDisplay.innerHTML = '';
+});
+
+// Make the delete button erase the last digit
+
+const deletePress = document.querySelector('#delete');
+deletePress.addEventListener('click', e => {
+    screenDisplay.innerHTML = screenDisplay.innerHTML.substring(0, screenDisplay.innerHTML.length - 1);
 })
